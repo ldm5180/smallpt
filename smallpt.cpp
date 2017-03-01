79128b0c89121364aa92907eb997f834a7260233
Lenny Maiorani <ldm5180@gmail.com>
2017-03-01 00:48:53 +0000

    Get rid of vector constructor.

--- smallpt.cpp
+++ smallpt.cpp
@@ -5,22 +5,19 @@
 // Usage: time ./smallpt 5000 && xv image.ppm
 
 struct Vec {        
-  double x, y, z;   // position, also color (r,g,b)
-  Vec(double x_ = 0, double y_ = 0, double z_ = 0) {
-    x = x_;
-    y = y_;
-    z = z_;
-  }
-  Vec operator+(const Vec &b) const { return Vec(x + b.x, y + b.y, z + b.z); }
-  Vec operator-(const Vec &b) const { return Vec(x - b.x, y - b.y, z - b.z); }
-  Vec operator*(double b) const { return Vec(x * b, y * b, z * b); }
-  Vec mult(const Vec &b) const { return Vec(x * b.x, y * b.y, z * b.z); }
+  double x = 0.;
+  double y = 0.;
+  double z = 0.; // position, also color (r,g,b)
+  Vec operator+(const Vec &b) const { return {x + b.x, y + b.y, z + b.z}; }
+  Vec operator-(const Vec &b) const { return {x - b.x, y - b.y, z - b.z}; }
+  Vec operator*(double b) const { return {x * b, y * b, z * b}; }
+  Vec mult(const Vec &b) const { return {x * b.x, y * b.y, z * b.z}; }
   Vec &norm() { return *this = *this * (1 / sqrt(x * x + y * y + z * z)); }
   double dot(const Vec &b) const {
     return x * b.x + y * b.y + z * b.z;
   } // cross:
   Vec operator%(Vec &b) {
-    return Vec(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x);
+    return {y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x};
   }
 };
 struct Ray {
@@ -46,18 +43,18 @@ struct Sphere {
 };
 Sphere spheres[] = {
     // Scene: radius, position, emission, color, material
-    Sphere(1e5, Vec(1e5 + 1, 40.8, 81.6), Vec(), Vec(.75, .25, .25),
+    Sphere(1e5, {1e5 + 1, 40.8, 81.6}, {}, {.75, .25, .25},
            DIFF), // Left
-    Sphere(1e5, Vec(-1e5 + 99, 40.8, 81.6), Vec(), Vec(.25, .25, .75),
-           DIFF),                                                     // Rght
-    Sphere(1e5, Vec(50, 40.8, 1e5), Vec(), Vec(.75, .75, .75), DIFF), // Back
-    Sphere(1e5, Vec(50, 40.8, -1e5 + 170), Vec(), Vec(), DIFF),       // Frnt
-    Sphere(1e5, Vec(50, 1e5, 81.6), Vec(), Vec(.75, .75, .75), DIFF), // Botm
-    Sphere(1e5, Vec(50, -1e5 + 81.6, 81.6), Vec(), Vec(.75, .75, .75),
-           DIFF),                                                      // Top
-    Sphere(16.5, Vec(27, 16.5, 47), Vec(), Vec(1, 1, 1) * .999, SPEC), // Mirr
-    Sphere(16.5, Vec(73, 16.5, 78), Vec(), Vec(1, 1, 1) * .999, REFR), // Glas
-    Sphere(600, Vec(50, 681.6 - .27, 81.6), Vec(12, 12, 12), Vec(),
+    Sphere(1e5, {-1e5 + 99, 40.8, 81.6}, {}, {.25, .25, .75},
+           DIFF),                                            // Rght
+    Sphere(1e5, {50, 40.8, 1e5}, {}, {.75, .75, .75}, DIFF), // Back
+    Sphere(1e5, {50, 40.8, -1e5 + 170}, {}, {}, DIFF),       // Frnt
+    Sphere(1e5, {50, 1e5, 81.6}, {}, {.75, .75, .75}, DIFF), // Botm
+    Sphere(1e5, {50, -1e5 + 81.6, 81.6}, {}, {.75, .75, .75},
+           DIFF),                                             // Top
+    Sphere(16.5, {27, 16.5, 47}, {}, Vec{1, 1, 1} * .999, SPEC), // Mirr
+    Sphere(16.5, {73, 16.5, 78}, {}, Vec{1, 1, 1} * .999, REFR), // Glas
+    Sphere(600, {50, 681.6 - .27, 81.6}, {12, 12, 12}, {},
            DIFF) // Lite
 };
 inline double clamp(double x) { return x < 0 ? 0 : x > 1 ? 1 : x; }
@@ -75,7 +72,7 @@ Vec radiance(const Ray &r, int depth, unsigned short *Xi) {
   double t;   // distance to intersection
   int id = 0; // id of intersected object
   if (!intersect(r, t, id))
-    return Vec();                  // if miss, return black
+    return {};                     // if miss, return black
   const Sphere &obj = spheres[id]; // the hit object
   Vec x = r.o + r.d * t, n = (x - obj.p).norm(),
       nl = n.dot(r.d) < 0 ? n : n * -1, f = obj.c;
@@ -89,7 +86,7 @@ Vec radiance(const Ray &r, int depth, unsigned short *Xi) {
   }
   if (obj.refl == DIFF) { // Ideal DIFFUSE reflection
     double r1 = 2 * M_PI * erand48(Xi), r2 = erand48(Xi), r2s = sqrt(r2);
-    Vec w = nl, u = ((fabs(w.x) > .1 ? Vec(0, 1) : Vec(1)) % w).norm(),
+    Vec w = nl, u = ((fabs(w.x) > .1 ? Vec{0, 1} : Vec{1}) % w).norm(),
         v = w % u;
     Vec d = (u * cos(r1) * r2s + v * sin(r1) * r2s + w * sqrt(1 - r2)).norm();
     return obj.e + f.mult(radiance(Ray(x, d), depth, Xi));
@@ -119,8 +116,8 @@ Vec radiance(const Ray &r, int depth, unsigned short *Xi) {
 }
 int main(int argc, char *argv[]) {
   int w = 1024, h = 768, samps = argc == 2 ? atoi(argv[1]) / 4 : 1; // # samples
-  Ray cam(Vec(50, 52, 295.6), Vec(0, -0.042612, -1).norm()); // cam pos, dir
-  Vec cx = Vec(w * .5135 / h), cy = (cx % cam.d).norm() * .5135, r,
+  Ray cam({50, 52, 295.6}, Vec{0, -0.042612, -1}.norm()); // cam pos, dir
+  Vec cx = Vec{w * .5135 / h}, cy = (cx % cam.d).norm() * .5135, r,
       *c = new Vec[w * h];
 #pragma omp parallel for schedule(dynamic, 1) private(r) // OpenMP
   for (int y = 0; y < h; y++) {                          // Loop over image rows
@@ -142,7 +139,7 @@ int main(int argc, char *argv[]) {
             r = r +
                 radiance(Ray(cam.o + d * 140, d.norm()), 0, Xi) * (1. / samps);
           } // Camera rays are pushed ^^^^^ forward to start in interior
-          c[i] = c[i] + Vec(clamp(r.x), clamp(r.y), clamp(r.z)) * .25;
+          c[i] = c[i] + Vec{clamp(r.x), clamp(r.y), clamp(r.z)} * .25;
         }
   }
   FILE *f = fopen("image.ppm", "w"); // Write image to PPM file.
