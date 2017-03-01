2f8e2a231b6df9a33ff1b8ca0b0fa5549c84c297
Lenny Maiorani <ldm5180@gmail.com>
2017-03-01 00:49:59 +0000

    Cleanup formatting and const-correctness of Vec.

--- smallpt.cpp
+++ smallpt.cpp
@@ -8,15 +8,19 @@ struct Vec {
   double x = 0.;
   double y = 0.;
   double z = 0.; // position, also color (r,g,b)
+
   Vec operator+(const Vec &b) const { return {x + b.x, y + b.y, z + b.z}; }
   Vec operator-(const Vec &b) const { return {x - b.x, y - b.y, z - b.z}; }
   Vec operator*(double b) const { return {x * b, y * b, z * b}; }
   Vec mult(const Vec &b) const { return {x * b.x, y * b.y, z * b.z}; }
   Vec &norm() { return *this = *this * (1 / sqrt(x * x + y * y + z * z)); }
+
   double dot(const Vec &b) const {
     return x * b.x + y * b.y + z * b.z;
-  } // cross:
-  Vec operator%(Vec &b) {
+  }
+  // cross:
+
+  Vec operator%(Vec &b) const {
     return {y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x};
   }
 };
